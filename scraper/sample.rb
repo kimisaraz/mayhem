3d3316fefc64e3d9ac6b3c323e7c73fb9ab046b7
Kimi SARAZU <kimisaraz@gmail.com>
2014-08-23 23:19:19 +0900

    Regard charset

--- scraper/sample.rb
+++ scraper/sample.rb
@@ -2,6 +2,13 @@ require 'open-uri'
 require 'nokogiri'
 
 uri = 'http://www.google.com'
-doc = Nokogiri::HTML(open(uri))
+
+charset = nil
+html = open(uri) do |f|
+  charset = f.charset
+  f.read
+end
+
+doc = Nokogiri::HTML(html, nil, charset)
 
 puts doc.title
