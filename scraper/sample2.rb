dd1933334b868934c7c01a8e4e806fdfedae190e
Kimi SARAZU <kimisaraz@gmail.com>
2014-08-29 00:39:01 +0900

    Fix garbled characters

--- scraper/sample2.rb
+++ scraper/sample2.rb
@@ -3,13 +3,9 @@ require 'nokogiri'
 
 uri = 'http://www.1101.com/darling_column/'
 
-charset = nil
-html = open(uri) do |f|
-  charset = f.charset
-  f.read
-end
+html = open(uri, 'r:Shift_JIS').read.encode('UTF-8', 'Shift_JIS')
 
-doc = Nokogiri::HTML(html, nil, charset)
+doc = Nokogiri::HTML(html, nil, 'UTF-8')
 doc.css('table:last-child td:nth-child(2) > a').each do |node|
   puts node.attribute('href').value
   puts node.inner_text
