578ea373036f7b637eb8f29f42bd340fa717c40a
Kimi SARAZU <kimisaraz@gmail.com>
2014-08-30 09:00:19 +0900

    Modularize code in sample2.rb

--- scraper/sample2.rb
+++ scraper/sample2.rb
@@ -1,12 +1,18 @@
 require 'open-uri'
 require 'nokogiri'
 
-uri = 'http://www.1101.com/darling_column/'
+module DarlingColumnParser
+  def self.extract_links
+    uri = 'http://www.1101.com/darling_column/'
 
-html = open(uri, 'r:Shift_JIS').read.encode('UTF-8', 'Shift_JIS')
+    html = open(uri, 'r:Shift_JIS').read.encode('UTF-8', 'Shift_JIS')
 
-doc = Nokogiri::HTML(html, nil, 'UTF-8')
-doc.css('table:last-child td:nth-child(2) > a').each do |node|
-  puts node.attribute('href').value
-  puts node.inner_text
+    doc = Nokogiri::HTML(html, nil, 'UTF-8')
+    doc.css('table:last-child td:nth-child(2) > a').each do |node|
+      puts node.attribute('href').value
+      puts node.inner_text
+    end
+  end
 end
+
+DarlingColumnParser.extract_links
