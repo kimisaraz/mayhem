3bacefdfe12694cf41e8f3104be1003c7cbcd842
Kimi SARAZU <kimisaraz@gmail.com>
2014-09-04 23:37:15 +0900

    Refactor

--- scraper/sample2.rb
+++ scraper/sample2.rb
@@ -5,12 +5,12 @@ class DarlingColumnParser
   def extract_destinations
     uri = 'http://www.1101.com/darling_column/'
 
-    html = open(uri, 'r:Shift_JIS').read.encode('UTF-8', 'Shift_JIS')
+    html = open(uri, 'r:Shift_JIS:UTF-8').read.encode('UTF-8', 'Shift_JIS')
 
     doc = Nokogiri::HTML(html, nil, 'UTF-8')
     doc.css('table:last-child td:nth-child(2) > a').each do |node|
-      puts node.attribute('href').value
-      puts node.inner_text
+      p node.inner_text
+      p node.attribute('href').value
     end
   end
 end
