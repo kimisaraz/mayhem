e9b4415ea7db242fc99af9da6956044e9f11d4e5
Kimi SARAZU <kimisaraz@gmail.com>
2014-09-05 08:57:57 +0900

    Refactor

--- scraper/sample2.rb
+++ scraper/sample2.rb
@@ -9,12 +9,18 @@ class DarlingColumnParser
 
     doc = Nokogiri::HTML(html, nil, 'UTF-8')
     doc.css('table:last-child td:nth-child(2) > a').each do |node|
-      p node.inner_text
-      p node.attribute('href').value
+      # p node.inner_text
+      destinations << node.attribute('href').value
     end
+  end
+
+  def destinations
+    @destinations ||= []
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  DarlingColumnParser.new.extract_destinations
+  parser = DarlingColumnParser.new
+  parser.extract_destinations
+  p parser.destinations
 end
