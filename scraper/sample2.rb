57a3caa97dfa37a7e47f248adaba9184ff3b0c9d
Kimi SARAZU <kimisaraz@gmail.com>
2014-09-06 23:43:27 +0900

    Define initialize

--- scraper/sample2.rb
+++ scraper/sample2.rb
@@ -2,9 +2,14 @@ require 'open-uri'
 require 'nokogiri'
 
 class DarlingColumnParser
-  def extract_destinations
-    uri = 'http://www.1101.com/darling_column/'
+  attr_accessor :uri, :destinations
 
+  def initialize
+    self.uri = 'http://www.1101.com/darling_column/'
+    self.destinations = []
+  end
+
+  def extract_destinations
     html = open(uri, 'r:Shift_JIS:UTF-8').read.encode('UTF-8', 'Shift_JIS')
 
     doc = Nokogiri::HTML(html, nil, 'UTF-8')
@@ -12,10 +17,6 @@ class DarlingColumnParser
       # p node.inner_text
       destinations << node.attribute('href').value
     end
-  end
-
-  def destinations
-    @destinations ||= []
   end
 end
 
