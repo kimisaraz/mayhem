e2e00bb2f76ea158fdebfbe3c68cbbc8fab0c2b8
Kimi SARAZU <kimisaraz@gmail.com>
2014-09-07 22:16:09 +0900

    Define extract_archive_uris

--- scraper/sample2.rb
+++ scraper/sample2.rb
@@ -2,11 +2,12 @@ require 'open-uri'
 require 'nokogiri'
 
 class DarlingColumnParser
-  attr_accessor :uri, :destinations
+  attr_accessor :uri, :destinations, :archive_uris
 
   def initialize
     self.uri = 'http://www.1101.com/darling_column/'
     self.destinations = []
+    self.archive_uris = []
   end
 
   def extract_destinations
@@ -18,10 +19,22 @@ class DarlingColumnParser
       destinations << node.attribute('href').value
     end
   end
+
+  def extract_archive_uris
+    html = open(uri, 'r:Shift_JIS:UTF-8').read.encode('UTF-8', 'Shift_JIS')
+
+    doc = Nokogiri::HTML(html, nil, 'UTF-8')
+     doc.css('table:last-child td:nth-child(1) a').each do |node|
+      href = node.attribute('href').value
+      archive_uris << href if href[/^archive/]
+    end
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   parser = DarlingColumnParser.new
   parser.extract_destinations
   p parser.destinations
+  parser.extract_archive_uris
+  p parser.archive_uris
 end
