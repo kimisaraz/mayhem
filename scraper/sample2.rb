d2cf8c43f0f49c1d7069a76e222f2175386a8e61
Kimi SARAZU <kimisaraz@gmail.com>
2014-09-09 23:37:24 +0900

    Modify extract_destinations to treat plural URIs

--- scraper/sample2.rb
+++ scraper/sample2.rb
@@ -2,26 +2,27 @@ require 'open-uri'
 require 'nokogiri'
 
 class DarlingColumnParser
-  attr_accessor :uri, :destinations
+  attr_accessor :base_uri, :destinations
 
   def initialize
-    self.uri = 'http://www.1101.com/darling_column/'
+    self.base_uri = 'http://www.1101.com/darling_column/'
     self.destinations = []
   end
 
-  def extract_destinations
-    html = open(uri, 'r:Shift_JIS:UTF-8').read.encode('UTF-8', 'Shift_JIS')
+  def extract_destinations(*uris)
+    uris.each do |uri|
+      html = open(uri, 'r:Shift_JIS:UTF-8').read.encode('UTF-8', 'Shift_JIS')
 
-    doc = Nokogiri::HTML(html, nil, 'UTF-8')
-    doc.css('table:last-child td:nth-child(2) > a').each do |node|
-      # p node.inner_text
-      destinations << node.attribute('href').value
+      doc = Nokogiri::HTML(html, nil, 'UTF-8')
+      doc.css('table:last-child td:nth-child(2) > a').each do |node|
+        destinations << node.attribute('href').value
+      end
     end
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   parser = DarlingColumnParser.new
-  parser.extract_destinations
+  parser.extract_destinations(parser.base_uri)
   p parser.destinations
 end
